1c65b8fd29fe6d808f04c1bb35eae503b3ecc400
Chansik Yun <chansigi@gmail.com>
2023-03-03 14:49:34 +0900

    .

--- 7_struct.cpp
+++ 7_struct.cpp
@@ -6,11 +6,21 @@ struct Point {
     int x;
     int y;
     double z;
+    // double xx;
+};
+
+struct Rect {
+    Point leftTop;
+    Point rightBottom;
 };
 
 int main()
 {
+    Rect r;
     Point pt = { 10, 20 };
+
+    auto [lt, rb] = r;
+    cout << lt.x << endl;
 
     // cout << pt.x << ", " << pt.y << endl;
     int x = pt.x;
@@ -20,6 +30,7 @@ int main()
 
     // C++17, Structured Binding 선언
     // 1) auto를 반드시 사용해야 합니다.
+    // 2) 멤버 데이터 개수와 동일하게 선언되어야 합니다.
     auto [a, b, c] = pt;
     cout << a << ", " << b << ", " << c << endl;
 }

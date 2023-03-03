a0a579be2d067991df7f8357f5bdbdb4c6e14d67
Chansik Yun <chansigi@gmail.com>
2023-03-03 16:39:21 +0900

    .

--- 9_함수5.cpp
+++ 9_함수5.cpp
@@ -2,6 +2,15 @@
 #include <iostream>
 using namespace std;
 
+// 1. inline 키워드를 사용해도, 인라인화 여부는
+//    컴파일러가 결정합니다.
+//  MSVC: /Ob1
+
+// 2. 인라인 최적화를 통해 호출되지 않는 함수의 기계어 코드는
+//    사라집니다.
+// 3. 인라인 함수는 코드 메모리 사용량의 원인이 될 수 있습니다.
+//   > 간결한 함수에 대한 인라인 치환이 좋습니다.
+
 int add(int a, int b) { return a + b; }
 
 int main()

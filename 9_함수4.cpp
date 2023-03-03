c235bf09abacb2b6968fd37b5f8462d4c5a5c580
Chansik Yun <chansigi@gmail.com>
2023-03-03 16:18:54 +0900

    .

---
+++ 9_함수4.cpp
@@ -0,0 +1,33 @@
+// 9_함수4.cpp
+#include <iostream>
+using namespace std;
+
+#if 0
+int square(int x) { return x * x; }
+
+int main()
+{
+    cout << square(10) << endl;
+    cout << 10 * 10 << endl;
+}
+#endif
+
+// 1. C 에서는 함수 호출을 최적화하기 위해서,
+//    매크로 함수를 사용합니다.
+#define SQUARE(x) ((x) * (x))
+#define DBL(x) ((x) + (x))
+
+//  - 우선순위 문제가 발생할 수 있습니다.
+//   => 매크로의 인자 영역과 치환 영역을 괄호를 통해
+//      해결할 수 있습니다.
+
+int main()
+{
+    int a = 10;
+
+    cout << SQUARE(10) << endl;
+    cout << SQUARE(a + a) << endl;
+
+    cout << DBL(a) * DBL(a) << endl;
+    // (a) + (a) * (a) + (a)
+}

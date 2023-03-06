a7dd3178fb9aee0ef7c62a7a918660016c350d32
Chansik Yun <chansigi@gmail.com>
2023-03-06 14:45:07 +0900

    .

--- 15_nullptr.cpp
+++ 15_nullptr.cpp
@@ -8,6 +8,8 @@ using namespace std;
 // 2. NULL은 정수인지 포인터인지 모호합니다.
 //    "타입 안정성이 떨어집니다."
 // 3. C++11에서 NULL을 대체하는 nullptr이 도입되었습니다.
+// 4. nullptr_t 라는 정확한 타입으로 설계되어 있지만,
+//    몰라도 됩니다.
 
 void foo(int n) { cout << "int" << endl; }
 void foo(int* p) { cout << "int*" << endl; }
@@ -17,6 +19,15 @@ int main()
     foo(0);
     // foo(NULL); /* 모호성 오류! */
     foo(nullptr);
+
+    char* p = nullptr;
+    if (p != nullptr) {
+    }
+
+    // nullptr_t는 bool 타입으로의 암묵적 변환이 허용되도록
+    // 설계되었습니다.
+    if (nullptr) {
+    }
 
     // int* p = 0;
     // int* p = NULL;

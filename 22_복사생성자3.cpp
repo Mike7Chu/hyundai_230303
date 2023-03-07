bd975e0489d608df05713f1c8e2dbf0d1114c118
Chansik Yun <chansigi@gmail.com>
2023-03-07 13:32:00 +0900

    .

--- 22_복사생성자3.cpp
+++ 22_복사생성자3.cpp
@@ -14,13 +14,14 @@ public:
 // 2. 지역 객체를 값으로 반환하면, 복사 생성의 비용이 발생합니다.
 // 3. RVO(Return Value Optimization)
 //   - 임시 객체로 반환하면, 복사 생성 비용을 최적화할 수 있습니다.
-
+// 4. NRVO(Named RVO)
+//   - msvc 2022
 Sample foo()
 {
     // Sample s;
     // return s;
 
-    return Sample();
+    return Sample(); // 임시 객체 생성 문법
 }
 
 int main()

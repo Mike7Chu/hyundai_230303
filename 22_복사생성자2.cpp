88459e0ddde4535bfa12ca65ea2358def4c6e45c
Chansik Yun <chansigi@gmail.com>
2023-03-07 13:16:02 +0900

    .

--- 22_복사생성자2.cpp
+++ 22_복사생성자2.cpp
@@ -10,6 +10,16 @@ public:
     Sample(const Sample&) { cout << "Sample(const Sample&)" << endl; }
 };
 
+// 1. 함수의 인자로 객체가 전달될 때(call by value)
+//   해결 방법: const&로 전달해야 합니다.
+
+// void foo(Sample s) { }
+void foo(const Sample& s) { }
+
 int main()
 {
+    Sample s;
+    cout << "------" << endl;
+    foo(s);
+    cout << "------" << endl;
 }

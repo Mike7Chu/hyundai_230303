1142ae5d4a332f6ea84f52a09b893f60df96d7cf
Chansik Yun <chansigi@gmail.com>
2023-03-08 10:53:52 +0900

    .

--- 28_상속3.cpp
+++ 28_상속3.cpp
@@ -12,6 +12,40 @@ using namespace std;
 //  2. protected: 외부 접근 불가능, 자식 클래스 접근 가능
 //  3. public: 외부 접근 가능
 
+//-----
+// 4. protected로 멤버 데이터를 만드는 것은 좋지 않습니다.
+//    멤버 데이터의 접근 지정은 private이 좋습니다.
+//    자식이 접근해야 하는 멤버 데이터에 대한 멤버 함수를 제공하는 것이
+//    좋습니다.
+
+class Animal {
+private:
+    int age;
+
+protected:
+    int GetAge() const { return age; }
+};
+
+class Dog : public Animal {
+    int color;
+
+public:
+    void PrintAge() const
+    {
+        // cout << age << endl;
+        cout << GetAge() << endl;
+    }
+};
+
+int main()
+{
+    Dog d;
+    d.PrintAge();
+
+    // cout << d.age << endl;
+}
+
+#if 0
 class Animal {
 protected:
     int age;
@@ -34,3 +68,4 @@ int main()
 
     // cout << d.age << endl;
 }
+#endif

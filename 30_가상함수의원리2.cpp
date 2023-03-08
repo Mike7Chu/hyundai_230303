048c3f1d149ec4adff368e63cfddd2e7051f4d11
Chansik Yun <chansigi@gmail.com>
2023-03-08 15:35:22 +0900

    .

--- 30_가상함수의원리2.cpp
+++ 30_가상함수의원리2.cpp
@@ -2,14 +2,18 @@
 #include <iostream>
 using namespace std;
 
+// 정적 바인딩과 동적 바인딩의 개념을 이해하기 위한 예제입니다.
+
 class AAA {
 public:
     virtual void foo() { cout << "AAA foo" << endl; } // 1
+    // void foo() { cout << "AAA foo" << endl; } // 1
 };
 
 class BBB {
 public:
     virtual void goo() { cout << "BBB goo" << endl; } // 2
+    // void goo() { cout << "BBB goo" << endl; } // 2
 };
 
 int main()

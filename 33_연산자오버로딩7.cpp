3ef989bc7a19eeec082af7aeda1576945bdb89d9
Chansik Yun <chansigi@gmail.com>
2023-03-09 13:40:56 +0900

    .

--- 33_연산자오버로딩7.cpp
+++ 33_연산자오버로딩7.cpp
@@ -7,20 +7,20 @@ class Integer {
     int value;
 
 public:
-    Integer(int v = 0)
+    inline Integer(int v = 0)
         : value(v)
     {
     }
 
     // ++i
-    Integer& operator++()
+    inline Integer& operator++()
     {
         ++value;
         return *this;
     }
 
     // i++
-    Integer operator++(int)
+    inline Integer operator++(int)
     {
         Integer temp(*this); // 현재의 상태를 복사 생성 합니다.
         ++value;
@@ -28,7 +28,7 @@ public:
         return temp;
     }
 
-    friend ostream& operator<<(ostream& os, const Integer& i)
+    friend inline ostream& operator<<(ostream& os, const Integer& i)
     {
         return os << i.value;
     }
@@ -38,11 +38,25 @@ int main()
 {
     int n1 = 1;
     ++n1;
+    /*
+        mov     DWORD PTR n1$[rsp], 1
+        mov     eax, DWORD PTR n1$[rsp]
+        inc     eax
+        mov     DWORD PTR n1$[rsp], eax
+    */
+
     n1++;
     cout << n1 << endl;
 
     Integer n2 = 1;
     ++n2;
+    /*
+        mov     DWORD PTR n2$[rsp], 1
+        mov     eax, DWORD PTR n2$[rsp]
+        inc     eax
+        mov     DWORD PTR n2$[rsp], eax
+    */
+
     // n2.operator++() {}
 
     n2++;

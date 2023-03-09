b599475f0cb50dc55bc98d2f7a4738a99c95953c
Chansik Yun <chansigi@gmail.com>
2023-03-09 11:47:41 +0900

    .

--- 33_연산자오버로딩5.cpp
+++ 33_연산자오버로딩5.cpp
@@ -14,17 +14,33 @@ public:
     }
 
     friend ostream& operator<<(ostream& os, const Point& pt);
+    friend istream& operator>>(istream& is, Point& pt);
 };
 
+// 사용자 정의 타입에 대해서
+// 표준 입출력 객체를 통해 값을 입력 받거나, 출력하는 것이 가능합니다.
+// => 중요합니다!
 ostream& operator<<(ostream& os, const Point& pt)
 {
     os << pt.x << ", " << pt.y;
     return os;
 }
 
+istream& operator>>(istream& is, Point& pt)
+{
+    is >> pt.x >> pt.y;
+    return is;
+}
+
 int main()
 {
     Point pt(10, 20);
+    cin >> pt;
+    // cin.operator>>(pt)
+    // cin.operator>>(Point&) => 멤버 함수는 제공 불가능합니다.
+
+    // istream& operator>>(cin, Point&)
+
     cout << pt;
     // 1. 멤버 함수 => 사용자가 추가할 수 없습니다.
     // cout.operator<<(pt)
